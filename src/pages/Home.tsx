6f114ff5ef9f7fd59708a7f0c1973f9bafd00323
onehake <colin.champdavoine@izi-solutions.fr>
2024-06-10 18:27:31 +0200

    portfolio

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,8 +4,7 @@ import { motion, useScroll, useTransform } from 'framer-motion'
 import React, { useRef } from 'react'
 
 import back from '../images/back.png'
-import back2 from '../images/back2.png'
-import background from '../images/background.png'
+import front from '../images/front.png'
 import middle from '../images/middle.png'
 
 const Layer: React.FC<{ speed: number; image: string; zIndex: number }> = ({ speed, image, zIndex }) => {
@@ -62,18 +61,19 @@ export function Home() {
   return (
     <div ref={ref} className="w-full h-[300vh]  overflow-hidden bg-black">
       <Layer speed={0.2} image={back} zIndex={2} />
-      <Layer speed={0.8} image={back2} zIndex={3} />
-      <Layer speed={1} image={middle} zIndex={5} />
-      <Layerfront speed={3} image={background} zIndex={6} />
+      {/* <Layer speed={0.8} image={back2} zIndex={3} /> */}
+      <Layer speed={3} image={middle} zIndex={5} />
+      <Layer speed={6} image={front} zIndex={20} />
+      {/* <Layerfront speed={3} image={front} zIndex={5} /> */}
 
-      {/* <div className="absolute inset-0  flex items-center justify-center"> */}
-      <motion.div
-        className="flex items-center justify-center absolute font-bold text-white text-7xl md:text-9xl z-5 h-screen w-full"
-        style={{ translateY: useTransform(useScroll().scrollYProgress, [0, 1], [0, 200]) }}
-      >
-        ONEHAKE
-      </motion.div>
-      {/* </div> */}
+      <div className="absolute inset-0  flex items-center justify-center">
+        <motion.div
+          className="flex items-center justify-center absolute font-bold text-white text-7xl md:text-9xl z-10 h-screen w-full"
+          style={{ translateY: useTransform(useScroll().scrollYProgress, [0, 1], [0, 200]) }}
+        >
+          ONEHAKE
+        </motion.div>
+      </div>
       <BackgroundLayer speed={0.1} />
     </div>
   )
